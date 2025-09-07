3246695bbe91df1d662719e369979038d739a14f
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-09-07 14:28:50 -0300

    fix: Fetch download correctly

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,7 @@ async def upload(file_uploads: list[UploadFile]):
 @app.get("/status/{request_id}")
 async def get_download_id(request_id: str):
     request = session.query(Request).filter_by(public_id=request_id).one_or_none()
-    download = session.query(Download).filter_by(request_id=request_id).one_or_none()
+    download = session.query(Download).filter_by(request_id=request.id).one_or_none()
 
     if request is None:
         return {"error": "Invalid request", "status": "error"}
