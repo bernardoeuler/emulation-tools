ddb5c27c5ef4f8e08f70aba7749ca6f373f577ce
Bernardo Euler <bernardoeulersantos@gmail.com>
2026-04-05 12:45:31 -0300

    refactor: Move Base declaration to base_model.py

--- api/config/database/base_model.py
+++ api/config/database/base_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer
+from sqlalchemy.orm import declarative_base
 
-from api.config.database.setup import Base
+Base = declarative_base()
 
 
 class BaseModel(Base):
--- api/config/database/setup.py
+++ api/config/database/setup.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 load_dotenv()
@@ -11,6 +11,5 @@ DB_URL = os.getenv("DB_URL")
 if not DB_URL:
     raise ValueError("DB_URL is not set in environment variables")
 
-Base = declarative_base()
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
