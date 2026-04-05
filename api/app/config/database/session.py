1be1f576336bb9cfd8066e119507624947c4317c
Bernardo Euler <bernardoeulersantos@gmail.com>
2026-04-05 14:42:20 -0300

    refactor: Update DATABASE_URL retrieval to provide default value

--- api/app/config/database/session.py
+++ api/app/config/database/session.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-if not DATABASE_URL:
+if DATABASE_URL == "":
     raise ValueError("DATABASE_URL is not set in environment variables")
 
 engine = create_engine(DATABASE_URL)
