5e5bf10ef28d3cfd52431b056da067d8b91af493
Bernardo Euler <bernardoeulersantos@gmail.com>
2026-04-05 17:05:05 -0300

    refactor: add created_at and updated_at fields to Base model

--- api/database/models.py
+++ api/database/models.py
@@ -38,6 +38,8 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
 
 
 class Download(Base):
@@ -47,8 +49,6 @@ class Download(Base):
     request_id: Mapped[int] = mapped_column(Integer, ForeignKey("requests.id"), nullable=False)
     status: Mapped[DownloadStatusEnum] = mapped_column(SQLEnum(DownloadStatusEnum, name="download_status_enum"), nullable=False)
     file_uri: Mapped[str] = mapped_column(String)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
     expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
     request: Mapped["Request"] = relationship("Request", back_populates="downloads")
@@ -60,7 +60,5 @@ class Request(Base):
     public_id: Mapped[str] = mapped_column(String(32), nullable=False)
     type: Mapped[RequestTypeEnum] = mapped_column(SQLEnum(RequestTypeEnum, name="request_type_enum"), nullable=False)
     status: Mapped[RequestStatusEnum] = mapped_column(SQLEnum(RequestStatusEnum, name="request_status_enum"), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
 
     downloads: Mapped[List["Download"]] = relationship("Download", back_populates="request")
